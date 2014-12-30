0a2603dabf14e75dfd0331fc4068bac530e2dae7
James Luberda <James Luberda@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-30 06:47:02 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/y2k-bug-simulator/tags/1.2@1056724 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jbl-y2k.php
+++ jbl-y2k.php
@@ -7,7 +7,7 @@ Plugin Name: Y2K Bug Simulator
 Plugin URI: http://sp.uconn.edu/~jbl00001/jbl-y2k.zip
 Description: Renders the year as if a Y2K bug were present
 Author: James Luberda
-Version: 1.1
+Version: 1.2
 Author URI: http://sp.uconn.edu/~jbl00001
 License: GPLv2 or later
 */
@@ -29,10 +29,59 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+define( 'DEBUG', false );
+
 // verify plugin is not being called directly
 if ( !function_exists( 'add_action' ) ) {
         echo "This is a plugin and only functions when called by WordPress\n";
         exit;
+}
+
+// set up plugin default option
+register_activation_hook( __FILE__, 'jbl_y2k_activate' );
+
+function jbl_y2k_activate() {
+	add_option( 'jbl_y2k_adminonly', 'Yes' );
+}
+
+add_action( 'admin_menu', 'jbl_y2k_add_page' );
+add_action( 'admin_init', 'jbl_y2k_admin_init' );
+
+function jbl_y2k_add_page() {
+        add_options_page( 'Y2K Bug Simulator', 'Y2K', 'manage_options', 'jbl_y2k', 'jbl_y2k_option_page' );
+}
+
+function jbl_y2k_option_page() {
+	?>
+	<div class="wrap">
+	<h2>Y2k Bug Simulator</h2>
+	<form action="options.php" method="post">
+	<?php settings_fields( 'jbl_y2k_options' ); ?>
+	<?php do_settings_sections( 'jbl_y2k' ); ?>
+	<input name="Submit" type="submit" value="Save Changes" />
+	</form>
+	</div>
+        <?php
+}
+
+function jbl_y2k_admin_init() {
+	register_setting( 'jbl_y2k_options', 'jbl_y2k_adminonly' );
+	add_settings_section( 'jbl_y2k_main', 'Configuration Settings', 'jbl_y2k_section_text', 'jbl_y2k' );
+	add_settings_field( 'jbl_y2k_adminonly', 'Y2K Effect Admin-Only ', 'jbl_y2k_setting_input', 'jbl_y2k', 'jbl_y2k_main' );
+}
+
+// Draw the section header
+function jbl_y2k_section_text() {
+	echo "The following option determines which users see the Y2K date change. If checked, only admin-level users will see, i.e. '2014' rendered as '1914'";
+}
+
+// Display and fill the form field
+function jbl_y2k_setting_input() {
+	// get jbl_y2k_adminonly from the database
+	$jbl_y2k_adminonly = get_option( 'jbl_y2k_adminonly' );
+	$checked = $jbl_y2k_adminonly ? 'checked' : '';
+	// echo the field
+	echo "<input id='jbl_y2k_adminonly' name='jbl_y2k_adminonly' type='checkbox' value='Yes' $checked/>";
 }
 
 // list of filters
@@ -40,8 +89,6 @@ $jbl_y2k_filters = array(
 	'get_the_date',
 	'get_comment_date',
 	'get_the_modified_date',
-	'get_archives_link',
-	'get_calendar',
 	'wp_title',
 	'modified_edit_pre',
 	'modified_gmt_edit_pre',
@@ -50,13 +97,42 @@ $jbl_y2k_filters = array(
 	'post_date_column_time'
 );
 
-foreach ($jbl_y2k_filters as $jbl_y2k_filter) {
-	add_filter( $jbl_y2k_filter, 'jbl_y2k_convert');
+//list of archive filters
+$jbl_y2k_archive_filters = array(
+	'get_archives_link',
+	'get_calendar',
+);
+
+foreach ( $jbl_y2k_filters as $jbl_y2k_filter ) {
+	add_filter( $jbl_y2k_filter, 'jbl_y2k_convert' );
+}
+
+foreach ( $jbl_y2k_archive_filters as $jbl_y2k_archive_filter ) {
+	add_filter( $jbl_y2k_archive_filter, 'jbl_y2k_archive_convert' );
 }
 
 function jbl_y2k_convert( $content ) {
-        $content = preg_replace( "/(\D+?|^)20(\d\d(\D+?|$))/", "$1 19$2", $content );
+	if ( DEBUG ) {
+		$r_content_string = print_r ($content, true);
+		file_put_contents ( '/tmp/y2k-wp.log', $r_content_string, FILE_APPEND );
+	}
+	$jbl_y2k_adminonly = get_option( 'jbl_y2k_adminonly' );
+	if ( current_user_can( 'manage_options' ) || !$jbl_y2k_adminonly ) {
+	        $content = preg_replace( "/(\D+?|^)20(\d\d(\D+?|$))/", "$1 19$2", $content );
+	}
         return $content;
+}
+
+function jbl_y2k_archive_convert( $content ) {
+	if ( DEBUG ) {
+		$r_content_string = print_r ($content, true);
+		file_put_contents ( '/tmp/y2k-wp.log', $r_content_string, FILE_APPEND );
+	}
+	//kludgy change, just added a space before 20
+	if ( current_user_can( 'manage_options' ) || !$jbl_y2k_adminonly ) {
+	        $content = preg_replace( "/(\D+?|^) 20(\d\d(\D+?|$))/", "$1 19$2", $content );
+	}
+	return $content;
 }
 
 ?>
--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,9 @@
 <?php
-// stub uninstall file
-
 // If uninstall not called from WordPress exit
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit;
+
+// Delete option from options table
+delete_option( 'jbl_y2k_adminonly' );
+
 ?>
